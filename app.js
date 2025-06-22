abd9b7654bbd09d97175c07ea0253ba77ad5bb42
Manoj Dhage <manojdhage1341@gmail.com>
2025-06-22 09:12:57 +0530

    CRUD operation comit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("../Project-WanderLust-HotelBookingWebsite/Modules/listing")
 const path=require("path");
+const methodOverride=require("method-override");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -22,6 +23,7 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));//request data parse
+app.use(methodOverride("_method"));
 
 //Home route
 app.get("/", (req, res) => {
@@ -64,8 +66,30 @@ app.get("/listings/:id",async(req,res)=>{
     res.render("listings/show.ejs",{listing});
 })
 
+//Edit & Update Route\
+//GET /listings/:id/edit
+app.get("/listings/:id/edit",async (req,res)=>{
+       let {id}=req.params;
+       const listing=await Listing.findById(id);
+       res.render("listings/edit.ejs",{listing})
 
+})
 
+//update route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+
+})
+
+//Delete Route
+app.delete("/listings/:id",async (req,res)=>{
+     let {id}=req.params;
+     let deletedListing=await Listing.findByIdAndDelete(id);
+     console.log(deletedListing);
+     res.redirect("/listings");
+})
 app.listen(8080,() => {
     console.log("Server is listening to port 8080");
 });
