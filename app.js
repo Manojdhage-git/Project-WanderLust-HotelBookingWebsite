6baeb8453be8fe02bafa274ad3614d0491d8c2bb
Manoj Dhage <manojdhage1341@gmail.com>
2025-06-27 09:18:11 +0530

    Server side validation added to new form and edit form

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing=require("../Project-WanderLust-HotelBookingWebsite/Modules/listing
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate"); 
+const wrapAsync=require("./Utils/wrapAsync.js")
+const ExpressError=require("./Utils/ExpressError.js")
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -37,10 +39,10 @@ app.get("/", (req, res) => {
 
 //index route to show data or listings
 
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
         const allListings=await Listing.find({});
         res.render("listings/index.ejs",{allListings});
-});
+}));
 
 //from to create new listings
 //new & create route
@@ -52,47 +54,60 @@ app.get("/listings/new",(req,res)=>{
 })
 
 //Create route
-app.post("/listings",async (req,res)=>{
-    // let {title,description,image,price,country,location}=req.body;
-   const newListing=new Listing(req.body.listing);
+app.post("/listings",wrapAsync(async(req,res,next)=>{
+
+    const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-})
+}));
 
 
 //show route - show individual data of listings using id
 // /listings/:id - GET
 
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-})
+}));
 
 //Edit & Update Route\
 //GET /listings/:id/edit
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
        let {id}=req.params;
        const listing=await Listing.findById(id);
        res.render("listings/edit.ejs",{listing})
 
-})
+}));
 
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
 
-})
+}));
 
 //Delete Route
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id",wrapAsync(async (req,res)=>{
      let {id}=req.params;
      let deletedListing=await Listing.findByIdAndDelete(id);
      console.log(deletedListing);
      res.redirect("/listings");
-})
+}));
+
+// More explicit pattern matching:
+app.all(/.*/, (req, res, next) => {
+    next(new ExpressError(404, "page not found!"));
+});
+
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500, message="Something went wrong"}=err;
+        res.status(statusCode).send(message);
+});
+
+
 app.listen(8080,() => {
     console.log("Server is listening to port 8080");
 });
