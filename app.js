1abd0f5d77adef6edee7f60893c6a8ae11ebd9d9
Manoj Dhage <manojdhage1341@gmail.com>
2025-07-10 08:53:42 +0530

    Signup form added

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./Utils/ExpressError.js")
-const listings = require("./routes/listing.js")
-const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./Models/user.js");
+
+const listingsRouter = require("./routes/listing.js")
+const reviewsRouter = require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -60,7 +62,7 @@ app.use(flash());
 
 //authenticate
 app.use(passport.initialize());
-app.use(passport.session);
+app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -82,6 +84,8 @@ app.get("/", (req, res) => {
     res.render("listings/home.ejs");
 });
 
+
+//demo for authentication
 app.get("/demouser",async(req,res)=>{
     let fakeUser=new User({
         email:"abc@gmail.com",
@@ -92,8 +96,9 @@ app.get("/demouser",async(req,res)=>{
 })
 
 
-app.use("/listings", listings);
-app.use("/listings/:id", reviews);
+app.use("/listings", listingsRouter);
+app.use("/listings/:id", reviewsRouter);
+app.use("/",userRouter)
 
 
 // More explicit pattern matching
